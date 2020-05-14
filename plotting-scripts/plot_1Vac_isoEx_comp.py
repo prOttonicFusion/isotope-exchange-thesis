0e3bad0be92c050beac8b2d1a8f0f47b4044fe99
Otto <61689180+prOttonicFusion@users.noreply.github.com>
2020-05-15 02:15:40 +0300

    drop inverse isotope part

--- plotting-scripts/plot_1Vac_isoEx_comp.py
+++ plotting-scripts/plot_1Vac_isoEx_comp.py
@@ -22,7 +22,7 @@ def readData(fname, columnsToRead):
 tTH_isoEx_400K = readData(path+'1Vac_2000W_20H_6T_400K_3100ns/HinVac.out', [0,1,2])
 tTH_isoEx_450K = readData(path+'1Vac_2000W_20H_6T_450K_1200ns/HinVac.out', [0,1,2])
 tTH_isoEx_500K = readData(path+'1Vac_2000W_20H_6T_500K_200ns/HinVac.out', [0,1,2])
-tTH_isoEx_500K_inverse = readData(path+'1Vac_2000W_6H_20T_500K_600ns/HinVac.out', [0,1,2])
+#tTH_isoEx_500K_inverse = readData(path+'1Vac_2000W_6H_20T_500K_300ns/HinVac.out', [0,1,2])
 tT_diff_400K = readData(path+'1Vac_2000W_0H_6T_400K_3200ns/HinVac.out', [0,1])
 tT_diff_450K = readData(path+'1Vac_2000W_0H_6T_450K_1600ns/HinVac.out', [0,1]) 
 tT_diff_500K = readData(path+'1Vac_2000W_0H_6T_500K_800ns/HinVac.out', [0,1]) 
@@ -32,7 +32,7 @@ cf = 1e-6
 tTH_isoEx_400K[:,0] *= cf
 tTH_isoEx_450K[:,0] *= cf
 tTH_isoEx_500K[:,0] *= cf
-tTH_isoEx_500K_inverse[:,0] *= cf 
+#tTH_isoEx_500K_inverse[:,0] *= cf 
 tT_diff_400K[:,0] *= cf
 tT_diff_450K[:,0] *= cf
 tT_diff_500K[:,0] *= cf
@@ -42,7 +42,7 @@ tT_diff_500K[:,0] *= cf
 print('Drawing figures ...')
 
 # Figure dimensions [x, y]
-fsize = [6.0, 10.0]
+fsize = [6.0, 8]
 
 # Upper x-axis limit [ns]
 xulim = 3000.0
@@ -59,7 +59,7 @@ def plotter(x, y, isoExStle, lbl):
     plt.plot(x, y, **isoExStle, label=lbl)
 
 # 400 K
-plt.subplot(4,1,1)
+plt.subplot(3,1,1)
 init, final, step = [0,-1,10]
 plotter(tTH_isoEx_400K[init:final:step, 0], tTH_isoEx_400K[init:final:step,2], isoEx_style['H_iso'], isoEx_labels['H_iso'])
 plotter(tTH_isoEx_400K[init:final:step, 0], tTH_isoEx_400K[init:final:step,1], isoEx_style['T_iso'], isoEx_labels['T_iso'])
@@ -73,7 +73,7 @@ plt.text(400, 5.3, '(i) 400 K', font)
 plt.gca().axes.get_xaxis().set_ticklabels([]) # Hide x-axis tick labels
 
 # 450 K
-plt.subplot(4,1,2)
+plt.subplot(3,1,2)
 init, step, final = [0,10,-1]
 plotter(tTH_isoEx_450K[init:final:step, 0], tTH_isoEx_450K[init:final:step, 2], isoEx_style['H_iso'], isoEx_labels['H_iso'])
 plotter(tTH_isoEx_450K[init:final:step, 0], tTH_isoEx_450K[init:final:step, 1], isoEx_style['T_iso'], isoEx_labels['T_iso'])
@@ -87,7 +87,7 @@ plt.text(400, 5.3, '(ii) 450 K', font)
 plt.gca().axes.get_xaxis().set_ticklabels([]) # Hide x-axis tick labels
 
 # 500 K
-plt.subplot(4,1,3)
+plt.subplot(3,1,3)
 init, step, final = [0,10,-1]
 plotter(tTH_isoEx_500K[init:final:step, 0], tTH_isoEx_500K[init:final:step, 2], isoEx_style['H_iso'], isoEx_labels['H_iso'])
 plotter(tTH_isoEx_500K[init:final:step, 0], tTH_isoEx_500K[init:final:step, 1], isoEx_style['T_iso'], isoEx_labels['T_iso'])
@@ -98,17 +98,17 @@ plt.ylabel(ylbl)
 plt.text(400, 5.3, '(iii) 500 K', font)
 #leg = plt.legend()
 #leg.get_frame().set_linewidth(1.5*pltm)  # Legend bow linewidth
-plt.gca().axes.get_xaxis().set_ticklabels([]) # Hide x-axis tick labels
+#plt.gca().axes.get_xaxis().set_ticklabels([]) # Hide x-axis tick labels
 
 # 500 K; inverse isotopes
-plt.subplot(4,1,4)
-init, step, final = [0,10,-1]
-plotter(tTH_isoEx_500K_inverse[init:final:step, 0], tTH_isoEx_500K_inverse[init:final:step, 2], isoEx_style['H_iso'], isoEx_labels['H_iso'])
-plotter(tTH_isoEx_500K_inverse[init:final:step, 0], tTH_isoEx_500K_inverse[init:final:step, 1], isoEx_style['T_iso'], isoEx_labels['T_iso'])
-plt.xlim((1, xulim))
-plt.xlabel(xlbl)
-plt.ylabel(ylbl)
-plt.text(700, 5.3, '(iv) 500 K; inverted', font)
+#plt.subplot(4,1,4)
+#init, step, final = [0,10,-1]
+#plotter(tTH_isoEx_500K_inverse[init:final:step, 0], tTH_isoEx_500K_inverse[init:final:step, 2], isoEx_style['H_iso'], isoEx_labels['H_iso'])
+#plotter(tTH_isoEx_500K_inverse[init:final:step, 0], tTH_isoEx_500K_inverse[init:final:step, 1], isoEx_style['T_iso'], isoEx_labels['T_iso'])
+#plt.xlim((1, xulim))
+#plt.xlabel(xlbl)
+#plt.ylabel(ylbl)
+#plt.text(700, 5.3, '(iv) 500 K; inverted', font)
 #leg = plt.legend()
 #leg.get_frame().set_linewidth(1.5*pltm)  # Legend bow linewidth
 
@@ -126,7 +126,7 @@ def plotter(x, y, isoExStle, lbl):
     plt.semilogx(x, y, **isoExStle, label=lbl)
 
 # 400 K
-plt.subplot(4,1,1)
+plt.subplot(3,1,1)
 init, final, step = [0,-1,10]
 plotter(tTH_isoEx_400K[init:final:step, 0], tTH_isoEx_400K[init:final:step,2], isoEx_style['H_iso'], isoEx_labels['H_iso'])
 plotter(tTH_isoEx_400K[init:final:step, 0], tTH_isoEx_400K[init:final:step,1], isoEx_style['T_iso'], isoEx_labels['T_iso'])
@@ -140,7 +140,7 @@ leg.get_frame().set_linewidth(1.5*pltm)  # Legend bow linewidth
 plt.gca().axes.get_xaxis().set_ticklabels([]) # Hide x-axis tick labels
 
 # 450 K
-plt.subplot(4,1,2)
+plt.subplot(3,1,2)
 init, step, final = [0,10,-1]
 plotter(tTH_isoEx_450K[init:final:step, 0], tTH_isoEx_450K[init:final:step, 2], isoEx_style['H_iso'], isoEx_labels['H_iso'])
 plotter(tTH_isoEx_450K[init:final:step, 0], tTH_isoEx_450K[init:final:step, 1], isoEx_style['T_iso'], isoEx_labels['T_iso'])
@@ -154,7 +154,7 @@ plt.text(2, 5.3, '(ii) 450 K', font)
 plt.gca().axes.get_xaxis().set_ticklabels([]) # Hide x-axis tick labels
 
 # 500 K
-plt.subplot(4,1,3)
+plt.subplot(3,1,3)
 init, step, final = [0,10,-1]
 plotter(tTH_isoEx_500K[init:final:step, 0], tTH_isoEx_500K[init:final:step, 2], isoEx_style['H_iso'], isoEx_labels['H_iso'])
 plotter(tTH_isoEx_500K[init:final:step, 0], tTH_isoEx_500K[init:final:step, 1], isoEx_style['T_iso'], isoEx_labels['T_iso'])
@@ -165,19 +165,19 @@ plt.ylabel(ylbl)
 plt.text(2, 5.3, '(iii) 500 K', font)
 #leg = plt.legend()
 #leg.get_frame().set_linewidth(1.5*pltm)  # Legend bow linewidth
-plt.gca().axes.get_xaxis().set_ticklabels([]) # Hide x-axis tick labels
+#plt.gca().axes.get_xaxis().set_ticklabels([]) # Hide x-axis tick labels
 
 # 500 K; inverse isotopes
-plt.subplot(4,1,4)
-init, step, final = [0,10,-1]
-plotter(tTH_isoEx_500K_inverse[init:final:step, 0], tTH_isoEx_500K_inverse[init:final:step, 2], isoEx_style['H_iso'], isoEx_labels['H_iso'])
-plotter(tTH_isoEx_500K_inverse[init:final:step, 0], tTH_isoEx_500K_inverse[init:final:step, 1], isoEx_style['T_iso'], isoEx_labels['T_iso'])
-plt.xlim((1, xulim))
-plt.xlabel(xlbl)
-plt.ylabel(ylbl)
-plt.text(2, 5.3, '(iv) 500 K; inverted', font)
-leg = plt.legend()
-leg.get_frame().set_linewidth(1.5*pltm)  # Legend bow linewidth
+#plt.subplot(4,1,4)
+#init, step, final = [0,10,-1]
+#plotter(tTH_isoEx_500K_inverse[init:final:step, 0], tTH_isoEx_500K_inverse[init:final:step, 2], isoEx_style['H_iso'], isoEx_labels['H_iso'])
+#plotter(tTH_isoEx_500K_inverse[init:final:step, 0], tTH_isoEx_500K_inverse[init:final:step, 1], isoEx_style['T_iso'], isoEx_labels['T_iso'])
+#plt.xlim((1, xulim))
+#plt.xlabel(xlbl)
+#plt.ylabel(ylbl)
+#plt.text(2, 5.3, '(iv) 500 K; inverted', font)
+#leg = plt.legend()
+#leg.get_frame().set_linewidth(1.5*pltm)  # Legend bow linewidth
 
 # Show & save figure                                                                                       
 plt.tight_layout()                                                                                   
