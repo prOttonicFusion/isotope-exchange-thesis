23bdc1f1900d72f35279fa253bb7dbb113e0e526
Otto <61689180+prOttonicFusion@users.noreply.github.com>
2020-07-24 16:49:57 +0300

    add 2Vac 400K plot

--- plotting-scripts/plot_2Vac_isoEx_comp.py
+++ plotting-scripts/plot_2Vac_isoEx_comp.py
@@ -11,11 +11,16 @@ path += '2Vac_isoEx_results/'
 # HinVac.out format: Timestep NTinVac NHinVac NTinCell
 tTH_isoEx = readData(path+'2Vac_2000W_20H_10T_500K_500ns/HinVac.out', [0,1,2])
 tTH_diff  = readData(path+'2Vac_2000W_0H_10T_500K_1000ns/HinVac.out', [0,1,2])
+tTH_isoEx_400K = readData(path+'2Vac_2000W_20H_10T_400K_500ns/HinVac.out', [0,1,2])
+tT_diff_400K  = readData(path+'2Vac_2000W_0H_10T_400K_500ns/HinVac.out', [0,1,2])
+
 
 # Convert timesteps --> time in ns
 cf = 1e-6
 tTH_isoEx[:,0] *= cf
 tTH_diff[:,0]  *= cf
+tTH_isoEx_400K[:,0] *= cf
+tT_diff_400K[:,0]  *= cf
 
 
 
@@ -23,7 +28,10 @@ tTH_diff[:,0]  *= cf
 print('Drawing figures ...')
 
 # Figure dimensions [x, y]
-fsize = [6.0, 3.3]
+fsize = [6.0, 7.0]
+
+# Upper x-axis limit [ns]
+xulim = 500.0
 
 # axis labels
 ylbl = 'Atoms bound to Vac'
@@ -37,14 +45,29 @@ def plotter(x, y, isoExStle, lbl):
     plt.plot(x, y, **isoExStle, label=lbl)
 
 init, final, step = [0,-1,70]
+# 400 K
+plt.subplot(2,1,1)
+plotter(tT_diff_400K[:, 0], tT_diff_400K[:, 1], isoEx_style['T_diff'], isoEx_labels['T_diff'])
+plotter(tTH_isoEx_400K[init:final:step, 0], tTH_isoEx_400K[init:final:step, 2], isoEx_style['H_iso'], isoEx_labels['H_iso'])
+plotter(tTH_isoEx_400K[init:final:step, 0], tTH_isoEx_400K[init:final:step, 1], isoEx_style['T_iso'], isoEx_labels['T_iso'])
+plt.xlim((1, xulim))
+plt.ylim((0,12))
+plt.ylabel(ylbl)
+plt.text(50, 10.8, '(i) 400 K', font)
+#leg = plt.legend(loc='lower right', bbox_to_anchor=(0.99, 0.03))
+#leg.get_frame().set_linewidth(1.5*pltm)  # Legend bow linewidth
+plt.gca().axes.get_xaxis().set_ticklabels([]) # Hide x-axis tick labels
+
+# 500 K
+plt.subplot(2,1,2)
 plotter(tTH_isoEx[init:final:step, 0], tTH_isoEx[init:final:step,2], isoEx_style['H_iso'], isoEx_labels['H_iso'])
 plotter(tTH_isoEx[init:final:step, 0], tTH_isoEx[init:final:step,1], isoEx_style['T_iso'], isoEx_labels['T_iso'])
 plotter(tTH_diff[:, 0], tTH_diff[:, 1], isoEx_style['T_diff'], isoEx_labels['T_diff'])
-plt.xlim((1, 500))
-plt.ylim((1,12))
+plt.xlim((1, xulim))
+plt.ylim((0,12))
 plt.xlabel(xlbl)
 plt.ylabel(ylbl)
-plt.text(50, 10.8, '2-Vac; 500K', font)
+plt.text(50, 10.8, '(ii) 500K', font)
 
 
 # Show & save figure
@@ -60,14 +83,29 @@ def plotter(x, y, isoExStle, lbl):
     plt.semilogx(x, y, **isoExStle, label=lbl)
 
 init, final, step = [0,-1,70]
+# 400 K
+plt.subplot(2,1,1)
+plotter(tT_diff_400K[:, 0], tT_diff_400K[:, 1], isoEx_style['T_diff'], isoEx_labels['T_diff'])
+plotter(tTH_isoEx_400K[init:final:step, 0], tTH_isoEx_400K[init:final:step, 2], isoEx_style['H_iso'], isoEx_labels['H_iso'])
+plotter(tTH_isoEx_400K[init:final:step, 0], tTH_isoEx_400K[init:final:step, 1], isoEx_style['T_iso'], isoEx_labels['T_iso'])
+plt.xlim((1, xulim))
+plt.ylim((0,12))
+plt.ylabel(ylbl)
+plt.text(2, 10.8, '(i) 400 K', font)
+#leg = plt.legend(loc='lower right', bbox_to_anchor=(0.99, 0.03))
+#leg.get_frame().set_linewidth(1.5*pltm)  # Legend bow linewidth
+plt.gca().axes.get_xaxis().set_ticklabels([]) # Hide x-axis tick labels
+
+# 500 K
+plt.subplot(2,1,2)
 plotter(tTH_isoEx[init:final:step, 0], tTH_isoEx[init:final:step,2], isoEx_style['H_iso'], isoEx_labels['H_iso'])
 plotter(tTH_isoEx[init:final:step, 0], tTH_isoEx[init:final:step,1], isoEx_style['T_iso'], isoEx_labels['T_iso'])
 plotter(tTH_diff[:, 0], tTH_diff[:, 1], isoEx_style['T_diff'], isoEx_labels['T_diff'])
-plt.xlim((1, 500))
-plt.ylim((1,12))
+plt.xlim((1, xulim))
+plt.ylim((0,12))
 plt.xlabel(xlbl)
 plt.ylabel(ylbl)
-plt.text(2, 10.8, '2-Vac; 500K', font)
+plt.text(2, 10.8, '(ii) 500K', font)
 leg = plt.legend(loc='center left', bbox_to_anchor=(0.03, 0.42))
 leg.get_frame().set_linewidth(1.5*pltm)  # Legend bow linewidth
 
